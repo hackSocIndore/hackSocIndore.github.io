fd8bba15ec778202fb0f4366cb923652bdf86387
sunidhi14 <44546187+sunidhi14@users.noreply.github.com>
2018-10-28 20:47:04 +0530

    this is basic program
    this is a c++ program

---
+++ basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"enter two numbers:";
+cin>>a>>b;
+if(a>b)
+cout<<"largest between two is:"<<a;
+else
+cout<<b<<"is greater";
+return 0;
+}
