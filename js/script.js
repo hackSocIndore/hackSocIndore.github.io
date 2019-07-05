0de62b3316f81a4f71c4678135ba84be095f6ca1
vbhv98 <vaibhavbhawalkar98.vb@gmail.com>
2019-07-05 17:01:46 +0530

    Navbar Bug Fixed!

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $(".fixed-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-        var $text = $(".text-white");
+$(function() {
+    $(document).scroll(function() {
+        var $nav = $('.fixed-top')
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height())
+        var $text = $('.text-white')
         $text.toggleClass('dark', $(this).scrollTop() > $nav.height())
-    });
-});
+    })
+    $('.navbar-toggler').click(() => {
+        var $nav = $('.fixed-top')
+        var $text = $('.text-white')
+        if ($(this).scrollTop() < $nav.height()) {
+            $nav.toggleClass('scrolled')
+            $text.toggleClass('dark')
+        }
+    })
+})
