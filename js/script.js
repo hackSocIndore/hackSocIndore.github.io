b5b0a0897d6d688af2083b04ee61714efc72dd91
vbhv98 <vaibhavbhawalkar98.vb@gmail.com>
2019-08-06 15:39:10 +0530

    Index HTML made Responsive with carousel

--- js/script.js
+++ js/script.js
@@ -1,14 +1,45 @@
-$(function () {
+$(function() {
+    var mobileImage = ["img/event_posters/posterMob2.webp", "img/event_posters/bodygit.webp", "img/event_posters/slide1mob.webp", "img/event_posters/posterMob1.webp"];
+    var desktopImage = ["img/event_posters/New_Project.png", "img/event_posters/slide3_1.webp", "img/event_posters/git871.webp", "img/event_posters/slide1.webp", "img/event_posters/poster1.webp"];
+
     var $nav = $("#nav-bar");
-    $(document).scroll(function () {
+    var carousel = $(".carousel-inner");
+    var carouselIndicators = $(".carousel-indicators");
+
+    $(document).scroll(function() {
         var $navSupportedContent = $("#navbarSupportedContent");
         var isShown = $navSupportedContent.attr("class").includes(" show");
-        if(!isShown)
-            $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+        if (!isShown) $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
     $("#toggler").click(() => {
         if ($(this).scrollTop() < $nav.height()) {
             $nav.toggleClass("scrolled");
         }
     });
+    var updateCarousel = imageList => {
+        carousel.empty();
+        var carouselItems = imageList
+            .map((src, i) => {
+                if (i === 0) return '<div class="carousel-item active"><img src=' + src + ' width="100%" /></div>';
+                return '<div class="carousel-item"><img src=' + src + ' width="100%" /></div>';
+            })
+            .join("");
+        var indicators = imageList
+            .map((_, i) => {
+                if (i === 0) return '<li data-target="#myCarousel" data-slide-to="' + i + '" class="active"></li>';
+                return '<li data-target="#myCarousel" data-slide-to="' + i + '"></li>';
+            })
+            .join("");
+        carousel.html(carouselItems);
+        carouselIndicators.html(indicators);
+    };
+    var onresizecheck = () => {
+        if (screen.width <= 800 || $(window).width() <= 800) {
+            updateCarousel(mobileImage);
+        } else {
+            updateCarousel(desktopImage);
+        }
+    };
+    onresizecheck();
+    $(window).resize(onresizecheck);
 });
