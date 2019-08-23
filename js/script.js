c18905e855a5359d2307e07354d23c664a883503
Aman <amansohani222@gmail.com>
2019-08-23 20:57:56 +0000

    Added Navbar Toggling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function () {
+$(document).ready(function () {
     var $nav = $("#nav-bar");
     $(document).scroll(function () {
         var $navSupportedContent = $("#navbarSupportedContent");
@@ -11,13 +11,32 @@ $(function () {
             $nav.toggleClass("scrolled");
         }
     });
-	
+  
+  var temp = $("#nav-bar").css("height");
+  var nav_height = Number(temp.substring(0, temp.length-2));
   // Smooth scrolling
   var scrollLink = $('.scroll');  
   scrollLink.click(function(e) {
     e.preventDefault();
     $('body,html').animate({
-      scrollTop: $(this.hash).offset().top - 66.86
+      scrollTop: $(this.hash).offset().top - nav_height
     }, 1000 );
   });
+
+  // Navbar Links Toggeling
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+    
+    scrollLink.each(function() {
+      
+      var sectionOffset = $(this.hash).offset().top - (nav_height+3);
+      
+      if ( sectionOffset <= scrollbarLocation ) {
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+      }
+    })
+    
+  });
+  
 });
