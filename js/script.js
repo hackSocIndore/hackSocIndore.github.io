6c53e8853fef7304d5a1eb9a9f2e35f2b091b9b6
Aman <amansohani222@gmail.com>
2019-08-24 09:15:15 +0000

    Navbar Toggeling added

--- js/script.js
+++ js/script.js
@@ -12,12 +12,30 @@ $(function () {
         }
     });
 	
+  var temp = $("#nav-bar").css("height");
+  var nav_height = Number(temp.substring(0, temp.length-2));
+  
   // Smooth scrolling
   var scrollLink = $('.scroll');  
   scrollLink.click(function(e) {
     e.preventDefault();
     $('body,html').animate({
-      scrollTop: $(this.hash).offset().top - 66.86
+      scrollTop: $(this.hash).offset().top - nav_height
     }, 1000 );
+  });
+
+  // Navbar Links Toggeling
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+    
+    scrollLink.each(function() {
+      
+      var sectionOffset = $(this.hash).offset().top - (nav_height+5);
+      
+      if ( sectionOffset <= scrollbarLocation ) {
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+      }
+    })
   });
 });
